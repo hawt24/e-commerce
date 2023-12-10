c58f871cf8c950fc2007e1243897470abda10e4c
konjoyeww <kemal.siraj@a2sv.org>
2023-12-10 10:37:54 +0300

    E-commerce-shoes

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import {FiHeart} from "react-icons/fi"
+import { AiOutlineShoppingCart,AiOutlineUserAdd } from 'react-icons/ai'
+
 import './Nav.css'
 
 const Nav = () => {
   return (
-    <div>
+    <nav>
+    <div className='nav-container'>
+        <input type='text' 
+        className='search-input'
+        placeholder='Enter your search shoes'
+        />
       
     </div>
+    <div className='profile-container'>
+        <a href='#'>
+            <FiHeart className='nav-icons'>
+
+            </FiHeart>
+        </a>
+        <a href='#'>
+            <AiOutlineShoppingCart className='nav-icons'>
+            </AiOutlineShoppingCart>
+        </a>
+        <a href='#'>
+            <AiOutlineUserAdd className='nav-icons'>
+            </AiOutlineUserAdd>
+
+        </a>
+    </div>
+    </nav>
+
   )
 }
 
--- src/Product/product.js
+++ src/Product/product.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import {AiFillStar} from "react-icons/ai"
+import {BsFillBagCheckFill} from "react-icons/bs"
+import "./product.css"
 
 const product = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='card-container'>
+      <section className='card'>
+        <img
+        className='card-img '
+        src='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
+        alt='shoe'
+        />
+        <div className='card-details'>
+          <h3 className='card-title'>Shoe</h3>
+        </div>
+        <section className='card-reviews'>
+          <AiFillStar className='rating-star'/> <AiFillStar className="rating-star"/> <AiFillStar className='rating-star'/> <AiFillStar className='rating-star'/>
+          <span className='total-reviews'>4</span>
+        </section>
+        <section>
+          <div className='card-price'>
+            <del>$300</del>200
+          </div>
+          <div className='bag'>
+          <BsFillBagCheckFill/>
+          </div>
+        </section>
+      </section>
+    </section>
   )
 }
 
